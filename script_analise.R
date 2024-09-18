56f4abaa61db190d247003dfebef1403b1148e66
Ester Costa de Souza <alwesterc@gmail.com>
2024-09-18 17:44:30 -0300

    first commit

---
+++ script_analise.R
@@ -0,0 +1,12 @@
+# Você foi designado como analista de desempenho de uma equipe de vendas e precisa analisar os resultados de uma campanha recente. Utilizando R e GitHub, você deverá criar um script que permita:
+
+# Criar um vetor com as vendas diárias de 10 vendedores: Cada venda diária será representada por um número (quantidade de vendas).
+
+# Verificar quais vendedores atingiram a meta mínima de 20 vendas: Use operadores lógicos para identificar quais vendedores cumpriram a meta.
+
+# Calcular quantos vendedores atingiram a meta: Use coerção lógica para somar os vendedores que bateram a meta.
+
+# Substituir qualquer valor de vendas inferior a 15 por 15: Isso representa um ajuste para que os vendedores com desempenho muito baixo ainda possam melhorar.
+
+# Gerar um “relatório” de desempenho: Para cada vendedor, informe se ele atingiu a meta ou não.
+
